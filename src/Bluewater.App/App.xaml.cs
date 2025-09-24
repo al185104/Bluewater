29ed096c3ae73bc90b854f648abd8aec0807c15a
al185104 <adrian.llamido@gmail.com>
2025-09-24 14:35:13 +0200

    add .net maui app

---
+++ src/Bluewater.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Bluewater.App;
+
+public partial class App : Application
+{
+  public App()
+  {
+    InitializeComponent();
+  }
+
+  protected override Window CreateWindow(IActivationState? activationState)
+  {
+    return new Window(new AppShell());
+  }
+}
---
+++ src/Bluewater.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Bluewater.App;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
