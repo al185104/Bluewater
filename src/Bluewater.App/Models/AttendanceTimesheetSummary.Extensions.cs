be863ef05911cb5836197ca2b28d6115dea16d0c
Adrian <adrian.llamido@gmail.com>
2025-11-02 15:11:03 +0100

    Align timesheet layout with schedule and show date pickers

--- src/Bluewater.App/Models/AttendanceTimesheetSummary.Extensions.cs
+++ src/Bluewater.App/Models/AttendanceTimesheetSummary.Extensions.cs
@@ -9,8 +9,28 @@ public partial class AttendanceTimesheetSummary
   public bool HasTimeIn2 => TimeIn2.HasValue;
   public bool HasTimeOut2 => TimeOut2.HasValue;
 
+  public DateTime TimeIn1Date => GetDateOrFallback(TimeIn1);
+  public DateTime TimeOut1Date => GetDateOrFallback(TimeOut1);
+  public DateTime TimeIn2Date => GetDateOrFallback(TimeIn2);
+  public DateTime TimeOut2Date => GetDateOrFallback(TimeOut2);
+
   public TimeSpan TimeIn1Time => TimeIn1?.TimeOfDay ?? TimeSpan.Zero;
   public TimeSpan TimeOut1Time => TimeOut1?.TimeOfDay ?? TimeSpan.Zero;
   public TimeSpan TimeIn2Time => TimeIn2?.TimeOfDay ?? TimeSpan.Zero;
   public TimeSpan TimeOut2Time => TimeOut2?.TimeOfDay ?? TimeSpan.Zero;
+
+  private DateTime GetDateOrFallback(DateTime? value)
+  {
+    if (value is DateTime dateTime)
+    {
+      return dateTime.Date;
+    }
+
+    if (EntryDate is DateOnly entryDate)
+    {
+      return entryDate.ToDateTime(TimeOnly.MinValue);
+    }
+
+    return DateTime.Today;
+  }
 }
